ba85388150f2508e1b8bfc5652c54fd14176b2df
Sahmwell <samuelandersonsss@gmail.com>
2021-03-01 17:08:41 -0600

    probably broken but delay and queue length seem okay

--- TrainingGym/benchmark.py
+++ TrainingGym/benchmark.py
@@ -4,13 +4,14 @@ import sumolib
 import time
 import random
 
-from typing import List, Dict
+from typing import List, Dict, Set
 from stable_baselines import PPO2
 from env.SumoEnvParallel import SumoEnvParallel
 from collections import defaultdict
 
 OUTPUT_TURN_COUNTS = False  # Set to True to Enable Turn Count Output
-USE_LEARNED_MODEL = True  # Set to True to Benchmark PPO2 Model
+USE_LEARNED_MODEL = False  # Set to True to Benchmark PPO2 Model
+OUTPUT_INTERSECTION_STATS = True  # Output Per Intersection Statistics
 SEED = 500 # Set to -1 for random seed
 
 def printProgressBar(iteration, total, prefix='Progress:', suffix='', decimals=1, length=50, fill='█'):
@@ -37,15 +38,37 @@ def printProgressBar(iteration, total, prefix='Progress:', suffix='', decimals=1
 
 
 def get_metrics(
-        connections: List[Dict], capacities: List[Dict], vehicles: Dict[str, float], t=traci,
-        wait_cache=defaultdict(int), total_wait_cache=defaultdict(int)
+        connections: List[Dict], capacities: List[Dict], vehicles: Dict[str, float], all_important_roads: Dict,
+        node_stats: Dict, t=traci, last_step_edge_cache=defaultdict(lambda: ""), wait_cache=defaultdict(int),
+        total_wait_cache=defaultdict(int), routes={}
 ):
     for v_id in set(t.vehicle.getIDList()).difference(set(t.simulation.getDepartedIDList()).union(
             *t.simulation.getStopStartingVehiclesIDList()).union(*t.simulation.getStopEndingVehiclesIDList())):
         curr_wait = t.vehicle.getWaitingTime(v_id)
+        edge = ""
+        if OUTPUT_INTERSECTION_STATS:
+            # Get Vehicle Edge
+            if v_id not in routes:
+                routes[v_id] = t.vehicle.getRoute(v_id)
+            route_index = t.vehicle.getRouteIndex(v_id)
+            edge = routes[v_id][route_index]
+            if edge in all_important_roads:
+                node_name = all_important_roads[edge]
+                node_stats[node_name]['v_ids'].add(v_id)
+
         if not curr_wait and wait_cache[v_id]:
             total_wait_cache[v_id] += wait_cache[v_id]
+
+            if OUTPUT_INTERSECTION_STATS:
+                # Get Vehicle Edge
+                last_edge = last_step_edge_cache[v_id]
+                # If the last edge is on an important road add the wait to that nodes wait time
+                if last_edge in all_important_roads:
+                    node_name = all_important_roads[last_edge]
+                    node_stats[node_name]['wait_sum'] += wait_cache[v_id]
         wait_cache[v_id] = curr_wait
+        if OUTPUT_INTERSECTION_STATS:
+            last_step_edge_cache[v_id] = edge
     for v_id in t.simulation.getArrivedIDList():
         if v_id not in vehicles:
             vehicles[v_id] = total_wait_cache[v_id]
@@ -64,14 +87,18 @@ def get_metrics(
                 [capacity['vehicles'].add(v_id) for v_id in t.lane.getLastStepVehicleIDs(lane)]
 
 
-def step(connections: List[Dict], capacities: List[Dict], total_steps: int, vehicles: Dict[str, float], env, model, obs):
+def step(
+        connections: List[Dict], capacities: List[Dict], total_steps: int, vehicles: Dict[str, float], env, model, obs,
+        all_important_roads, node_stats
+):
     if USE_LEARNED_MODEL:
         action, state = model.predict(obs)
         new_obs, _, _, _ = env.step(action)
     else:
         new_obs = None
         traci.simulationStep()
-    get_metrics(connections, capacities, vehicles, env.sumo if USE_LEARNED_MODEL else traci)
+    get_metrics(
+        connections, capacities, vehicles, all_important_roads, node_stats, env.sumo if USE_LEARNED_MODEL else traci)
 
     t = traci.simulation.getTime()
     printProgressBar(t, total_steps, suffix=f'Complete. Finished {t} of {total_steps} ')
@@ -101,6 +128,14 @@ def benchmark():
         capacity['vehicles'] = set()
     for direction in connections:
         direction['vehicles'], direction['queues'], direction['delays'] = set(), [], []
+    all_important_roads = {e: n['node_name'] for n in controlled_lights for d in n['connections'] for e in d['edges']}
+    node_stats = {}
+    for node in controlled_lights:
+        node_stats[node['node_name']] = {
+            'edges': {edge for direction in node['connections'] for edge in direction},
+            'wait_sum': 0,
+            'v_ids': set()
+        }
 
     env, model, obs = None, None, None
     if USE_LEARNED_MODEL:
@@ -117,7 +152,7 @@ def benchmark():
         traci.start([sumolib.checkBinary('sumo')] + load_options)
     t = traci if not USE_LEARNED_MODEL else env.sumo
     for i in range(total_steps):
-        obs = step(connections, capacities, total_steps, vehicles, env, model, obs)
+        obs = step(connections, capacities, total_steps, vehicles, env, model, obs, all_important_roads, node_stats)
         if not t.simulation.getMinExpectedNumber():
             print(f"Simulation ended at {t.simulation.getTime()}.")
             break
@@ -142,6 +177,24 @@ def benchmark():
     if OUTPUT_TURN_COUNTS:
         [print(f'Total Vehicles on {r["label"]}: {len(r["vehicles"])} Expected: {r["expected"]}') for r in connections]
         [print(f'Total Vehicles on {l["label"]}: {len(l["vehicles"])} Expected: {l["expected"]}') for l in capacities]
+
+    if OUTPUT_INTERSECTION_STATS:
+        all_node_directions = {key: [] for key in node_stats.keys()}
+        for direction in edges:
+            node_name = f"node_{direction['label'][0]}"
+            all_node_directions[node_name].append(direction)
+        for node, node_stat in node_stats.items():
+            directions = all_node_directions[node]
+            avg_node_overflow = sum(
+                [condition(len(direction['vehicles']), direction['capacity']) for direction in directions]
+            ) / (len(directions) if directions else 1)
+            avg_node_queue_length = sum([q_len for direction in directions for q_len in direction['queues']]) / (
+                sum([len(direction['queues']) for direction in directions]) if directions else 1)
+            avg_node_delay = node_stat['wait_sum'] / (len(node_stat['v_ids']) if len(node_stat['v_ids']) else 1)
+
+            print(f'{node}\n{"-"*10}\nAverage Overflow (%): {avg_node_overflow * 100}\n'
+                  f'Average Queue Length (Num Cars): {avg_node_queue_length}\n'
+                  f'Average Delay: {avg_node_delay}\n{"-"*20}')
 
 
 benchmark()
